6e616803ca473f6237dd4752dfab20d592f0e7ea
xavierdd <128188503+xavierdd@users.noreply.github.com>
2023-03-18 13:17:53 -0400

    Add files via upload

---
+++ eventos/client/ready.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+const config = require('../../config/config.json');
+module.exports = client => {
+    //Nos conectamos a la base de datos
+
+    let palo = 53;
+
+    mongoose.connect(config.mongodb, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    }).then(() => {
+        console.log(`
+╔═════════════════════════════════════════════════════╗
+║                                                     ║
+║       Conectado a la base de datos de MONGODB!      ║
+║                                                     ║
+╚═════════════════════════════════════════════════════╝`.blue)
+    }).catch((err) => {
+        console.log(`☁ ERROR AL CONECTAR A LA BASE DE DATOS DE MONGODB`.red);
+        console.log(err)
+    })
+
+    console.log(`╔═════════════════════════════════════════════════════╗`.green)
+    console.log(`║ `.green + " ".repeat(-1 + palo - 1) + " ║".green)
+    console.log(`Conectado como ${client.user.tag}`)
+    console.log(`║ `.green + " ".repeat(-1 + palo - 1) + " ║".green)
+    console.log(`╚═════════════════════════════════════════════════════╝`.green)
+}
+
+/*console.log(`Conectado como ${client.uset.tag}`)
+╔═════════════════════════════════════════════════════╗
+║    || - || Desarrollado por dewstouh#1088 || - ||   ║
+║    ----------| discord.gg/MBPsvcphGf |----------    ║
+╚═════════════════════════════════════════════════════╝
+*/
